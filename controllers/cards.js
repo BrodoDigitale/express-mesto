3f21540391ae108c660d1646553d94c9ba8157c4
Julia Sensi <sensi.julia@gmail.com>
2021-11-16 12:38:44 +0100

    linter errors corrected

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,37 +1,37 @@
 const Card = require('../models/card');
-const { notFoundErrorStatus, generalErrorStatus, invalidDataErrorStatus } = require('../utils')
+const { notFoundErrorStatus, generalErrorStatus, invalidDataErrorStatus } = require('../utils');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .orFail(new Error ('Not found'))
+    .orFail(new Error('Not found'))
     .then((cards) => res.status(200).send(cards))
     .catch((err) => {
       if (err.message === 'Not found') {
-        res.status(notFoundErrorStatus).send({ message: 'Карточки не найдены'});
+        res.status(notFoundErrorStatus).send({ message: 'Карточки не найдены' });
         return;
       }
-      res.status(generalErrorStatus).send({ message: `Произошла ошибка ${err.name}: ${err.message}`});
+      res.status(generalErrorStatus).send({ message: `Произошла ошибка ${err.name}: ${err.message}` });
     });
 };
 
 module.exports.deleteCard = (req, res) => {
   const { cardId } = req.params;
   Card.findById(cardId)
-    .orFail(new Error ('Not found'))
+    .orFail(new Error('Not found'))
     .then((card) => {
-        card.delete();
-        return res.status(200).send({ message: 'Пост удален' })
-      })
+      card.delete();
+      return res.status(200).send({ message: 'Пост удален' });
+    })
     .catch((err) => {
       if (err.message === 'Not found') {
-        res.status(notFoundErrorStatus).send({ message: 'Карточка не найдена'});
+        res.status(notFoundErrorStatus).send({ message: 'Карточка не найдена' });
         return;
       }
-      if (err.name === "CastError") {
-        res.status(invalidDataErrorStatus).send({ message: 'Невалидный id'});
+      if (err.name === 'CastError') {
+        res.status(invalidDataErrorStatus).send({ message: 'Невалидный id' });
         return;
       }
-      res.status(generalErrorStatus).send({ message: `Произошла ошибка ${err.name}: ${err.message}`});
+      res.status(generalErrorStatus).send({ message: `Произошла ошибка ${err.name}: ${err.message}` });
     });
 };
 
@@ -41,46 +41,52 @@ module.exports.createCard = (req, res) => {
   Card.create({ name, link, owner }, { runValidators: true })
     .then((card) => res.status(200).send(card))
     .catch((err) => {
-      if (err.name === "ValidationError") {
-        res.status(invalidDataErrorStatus).send({ message: 'Введены некорректные данные'});
+      if (err.name === 'ValidationError') {
+        res.status(invalidDataErrorStatus).send({ message: 'Введены некорректные данные' });
         return;
       }
-      res.status(generalErrorStatus).send({ message: `Произошла ошибка ${err.name}: ${err.message}`});
+      res.status(generalErrorStatus).send({ message: `Произошла ошибка ${err.name}: ${err.message}` });
     });
 };
 
 module.exports.likeCard = (req, res) => {
-  Card.findByIdAndUpdate(req.params.cardId,
-    { $addToSet: { likes: req.user._id } }, { new: true })
-    .orFail(new Error ('Not found'))
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .orFail(new Error('Not found'))
     .then((card) => res.status(200).send(card))
     .catch((err) => {
       if (err.message === 'Not found') {
         res.status(notFoundErrorStatus).send({ message: 'Карточка не найдена' });
         return;
       }
-      if (err.name === "CastError") {
-        res.status(invalidDataErrorStatus).send({ message: 'Введены некорректные данные'});
+      if (err.name === 'CastError') {
+        res.status(invalidDataErrorStatus).send({ message: 'Введены некорректные данные' });
         return;
       }
-      res.status(generalErrorStatus).send({ message: `Произошла ошибка ${err.name}: ${err.message}`});
+      res.status(generalErrorStatus).send({ message: `Произошла ошибка ${err.name}: ${err.message}` });
     });
 };
 
 module.exports.dislikeCard = (req, res) => {
-  Card.findByIdAndUpdate(req.params.cardId,
-    { $pull: { likes: req.user._id } }, { new: true })
-    .orFail(new Error ('Not found'))
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .orFail(new Error('Not found'))
     .then((card) => res.status(200).send(card))
     .catch((err) => {
       if (err.message === 'Not found') {
         res.status(notFoundErrorStatus).send({ message: 'Карточка не найдена' });
         return;
       }
-      if (err.name === "CastError") {
-        res.status(invalidDataErrorStatus).send({ message: 'Введены некорректные данные'});
+      if (err.name === 'CastError') {
+        res.status(invalidDataErrorStatus).send({ message: 'Введены некорректные данные' });
         return;
       }
-      res.status(generalErrorStatus).send({ message: `Произошла ошибка ${err.name}: ${err.message}`});
+      res.status(generalErrorStatus).send({ message: `Произошла ошибка ${err.name}: ${err.message}` });
     });
 };
