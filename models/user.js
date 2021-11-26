166184d5ba54efc791b3a821a5c462bfdb5e9d62
Julia Sensi <sensi.julia@gmail.com>
2021-11-26 21:29:23 +0100

    complete controller createUser with email and password

--- models/user.js
+++ models/user.js
@@ -1,20 +1,41 @@
 const mongoose = require('mongoose');
+const { isEmail, isURL } = require('validator');
 
 // Схема пользователя
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
+    type: String,
+    required: false,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (v) => isURL(v),
+      message: 'Поле avatar не является ссылкой',
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неверно указан email',
+    },
+  },
+  password: {
     type: String,
     required: true,
   },
